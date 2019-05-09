658977c165d6f79f6ca176245306cb2ac0dad2da
Nat-Reid <nathanielreid@gmail.com>
2019-05-09 10:56:13 -0700

    link to recipe page

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 class RecipeCard extends Component{
+  handleLink = ev => {
+    ev.preventDefault()
+    window.open(this.props.url)
+  }
+
   render() {
     return (
-      <div>{this.props.name}</div>
+      <div><a onClick={this.handleLink} href={this.props.url}>{this.props.name}</a></div>
     );
   }
 }
